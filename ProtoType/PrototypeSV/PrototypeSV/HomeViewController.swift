25b4e8537bc4233edaf2ee4c1dcae1f97bb03aff
Hayunho <101854288+Hayunho@users.noreply.github.com>
2024-01-18 16:52:56 +0900

    edit: home화면 수정
    -추천 화면 비율 수정
    -추천 화면 애니메이션 추가
    -추천,영화,드라마 라벨 변경
    -추천,영화,드라마 목록 변경

--- ProtoType/PrototypeSV/PrototypeSV/HomeViewController.swift
+++ ProtoType/PrototypeSV/PrototypeSV/HomeViewController.swift
@@ -9,21 +9,32 @@ import UIKit
 
 class HomeViewController: ViewController {
  
-    let searchMovies: [String] = ["fiftyShadow", "woman", "bangjajoen", "downFall"]
-    let mzMovies: [String] = ["springOfSeoul", "SlamDunk", "HarryPoter1", "waterOfRoad"]
-    let recommendMovies: [String] = ["secret", "blackPanther", "trash", "crayonShinChan"]
+    let hotContents: [String] = ["starWars", "avengers", "avatar", "doctorWho", "CSI", "mentalist"]
+    let movies: [String] = ["HarryPotter1", "toyStory", "spiderman", "transformer", "caribbean", "cooperate"]
+    let dramas: [String] = ["squidGame", "doctor", "kimsabu", "Friends", "sweetHome", "walkingDead", "respond"]
+    
+    let miniScrollView = UIScrollView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.navigationController?.navigationBar.topItem?.title = "하윤호님 반갑습니다."
-        // self.navigationController?.hidesBarsOnSwipe = true
-        self.navigationController?.navigationBar.titleTextAttributes = [
-            NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 25),
-            NSAttributedString.Key.foregroundColor: UIColor.white
-        ]
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
+        imageView.contentMode = .scaleAspectFit
+        
+        let image = UIImage(named: "Title.001")
+        imageView.image = image
+        
+        navigationItem.titleView = imageView
         
         buildInterface()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        UIView.animate(withDuration: 2.5) {
+            self.miniScrollView.contentOffset = CGPoint(x: 280 * 5, y: 0)
+        }
     }
     
     func buildInterface() {
@@ -43,6 +54,8 @@ class HomeViewController: ViewController {
         
         scrollView.contentSize = CGSize(width: view.frame.width, height: 1000)
         
+        // 스크롤 뷰 오프셋 지정 후 애니메이션 지정하면 넷플릭스 메인 화면처럼 구성할 수있다.
+        
         let contentStackView = UIStackView()
         contentStackView.axis = .vertical
         contentStackView.spacing = 20
@@ -52,42 +65,39 @@ class HomeViewController: ViewController {
         scrollView.addSubview(contentStackView)
         
         NSLayoutConstraint.activate([
-            contentStackView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            contentStackView.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 20),
             contentStackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
             contentStackView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 10),
             contentStackView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -10)
         ])
-        // contentStackView 안쪽 내용들
         
+        // contentStackView 안쪽 내용들
         // MARK: - First Cell
         let label = UILabel()
-        label.text = "가장 검색을 많이 했던 영화"
+        label.text = "최근 핫한 컨텐츠들!"
         label.font = UIFont.preferredFont(forTextStyle: .title2)
         contentStackView.addArrangedSubview(label)
         
-        let miniScrollView = UIScrollView()
-
-        miniScrollView.contentSize = CGSize(width: 650, height: 200)
+        miniScrollView.contentSize = CGSize(width: 300 * hotContents.count, height: 400)
         miniScrollView.translatesAutoresizingMaskIntoConstraints = false
         
-        for i in 0 ... 3 {
+        for i in 0 ..< hotContents.count {
             let imageView = UIImageView()
-            imageView.image = UIImage(named: searchMovies[i])
-            imageView.frame = CGRect(x: 0 + (150 * i), y: 0, width: 140, height: 180)
+            imageView.image = UIImage(named: hotContents[i])
+            imageView.frame = CGRect(x: 0 + (300 * i), y: 0, width: 290, height: 400)
             miniScrollView.addSubview(imageView)
         }
         
         // 이미지 위에 덧씌워질 가상의 투명한 버튼
         let button = UIButton()
         button.backgroundColor = .clear
-//        button.alpha = 0.35
-        button.frame = CGRect(x: 0, y: 0, width:700, height: 200)
+        button.frame = CGRect(x: 0, y: 0, width: 300 * (hotContents.count), height: 400)
         button.addTarget(self, action: #selector(goToMovieScene), for: .touchUpInside)
         miniScrollView.addSubview(button)
         
         NSLayoutConstraint.activate([
-            miniScrollView.widthAnchor.constraint(equalToConstant: 1800),
-            miniScrollView.heightAnchor.constraint(equalToConstant: 200)
+            miniScrollView.widthAnchor.constraint(equalToConstant: 3000),
+            miniScrollView.heightAnchor.constraint(equalToConstant: 400)
         ])
         
         contentStackView.addArrangedSubview(miniScrollView)
@@ -101,12 +111,12 @@ class HomeViewController: ViewController {
         
         let miniScrollView2 = UIScrollView()
         miniScrollView2.backgroundColor = .clear
-        miniScrollView2.contentSize = CGSize(width: 650, height: 200)
+        miniScrollView2.contentSize = CGSize(width: 150 * movies.count, height: 200)
         miniScrollView2.translatesAutoresizingMaskIntoConstraints = false
         
-        for i in 0 ... 3 {
+        for i in 0 ..< movies.count {
             let imageView = UIImageView()
-            imageView.image = UIImage(named: mzMovies[i])
+            imageView.image = UIImage(named: movies[i])
             imageView.frame = CGRect(x: 0 + (150 * i), y: 0, width: 140, height: 180)
             miniScrollView2.addSubview(imageView)
         }
@@ -114,8 +124,7 @@ class HomeViewController: ViewController {
         // 이미지 위에 덧씌워질 가상의 투명한 버튼
         let button2 = UIButton()
         button2.backgroundColor = .clear
-//        button.alpha = 0.35
-        button2.frame = CGRect(x: 0, y: 0, width:700, height: 200)
+        button2.frame = CGRect(x: 0, y: 0, width: 150 * movies.count, height: 200)
         button2.addTarget(self, action: #selector(goToMovieScene), for: .touchUpInside)
         miniScrollView2.addSubview(button2)
         
@@ -132,18 +141,18 @@ class HomeViewController: ViewController {
         
         // MARK: - Third Cell
         let label3 = UILabel()
-        label3.text = "이걸 끝까지 안봐? 이걸 참아?"
+        label3.text = "드라마? 함 들라 마!"
         label3.font = UIFont.preferredFont(forTextStyle: .title2)
         contentStackView.addArrangedSubview(label3)
 
         let miniScrollView3 = UIScrollView()
         miniScrollView3.backgroundColor = .clear
-        miniScrollView3.contentSize = CGSize(width: 600, height: 200)
+        miniScrollView3.contentSize = CGSize(width:  150 * dramas.count, height: 200)
         miniScrollView3.translatesAutoresizingMaskIntoConstraints = false
         
-        for i in 0 ... 3 {
+        for i in 0 ..< dramas.count {
             let imageView = UIImageView()
-            imageView.image = UIImage(named: recommendMovies[i])
+            imageView.image = UIImage(named: dramas[i])
             imageView.frame = CGRect(x: 0 + (150 * i), y: 0, width: 140, height: 180)
             miniScrollView3.addSubview(imageView)
         }
@@ -151,8 +160,7 @@ class HomeViewController: ViewController {
         // 이미지 위에 덧씌워질 가상의 투명한 버튼
         let button3 = UIButton()
         button3.backgroundColor = .clear
-//        button.alpha = 0.35
-        button3.frame = CGRect(x: 0, y: 0, width:700, height: 200)
+        button3.frame = CGRect(x: 0, y: 0, width: 150 * dramas.count, height: 200)
         button3.addTarget(self, action: #selector(goToMovieScene), for: .touchUpInside)
         miniScrollView3.addSubview(button3)
         
@@ -174,8 +182,4 @@ class HomeViewController: ViewController {
         let nextViewController = FourthSceneViewController()
         self.navigationController?.pushViewController(nextViewController, animated: true)
     }
-}
-
-#Preview("HomeViewController"){
-    UINavigationController(rootViewController: HomeViewController())
 }
