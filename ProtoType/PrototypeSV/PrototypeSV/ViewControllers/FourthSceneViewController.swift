2b85ccf17f1cdba8970a87f8abfb2d451b21f81d
Taehoon Kang <kywlk0709@naver.com>
2024-01-18 17:50:03 +0900

    edit:영화장면 수정
    -영화장면 스택뷰 변경

--- ProtoType/PrototypeSV/PrototypeSV/ViewControllers/FourthSceneViewController.swift
+++ ProtoType/PrototypeSV/PrototypeSV/ViewControllers/FourthSceneViewController.swift
@@ -1,62 +1,105 @@
 //
-//  FourthSceneViewController.swift
+//  WatchMovieViewController.swift
 //  PrototypeSV
 //
-//  Created by 강 태훈 on 1/17/24.
+//  Created by 김성민 on 1/18/24.
 //
 
 import UIKit
 import WebKit
 
-class FourthSceneViewController: ViewController {
-    
-    let imageView: UIImageView = {
-        let image: UIImageView = UIImageView(image: UIImage(named: "FourthSceneImage"))
-        image.translatesAutoresizingMaskIntoConstraints = false
-        return image
-    }()
-    
-        
-    
+class FourthSceneViewController: UIViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.title = "해리윤호와 태훈님의 돌"
-        self.navigationController?.navigationBar.titleTextAttributes = [
-            NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 25),
-            NSAttributedString.Key.foregroundColor: UIColor.white
-        ]
-        
-        buildInterface()
+        self.title = "해리포터와 마법사의 돌"
+
+        setLayout()
     }
     
-    
-    func buildInterface() {
+    func setLayout() {
         let safeArea: UILayoutGuide = view.safeAreaLayoutGuide
         
-        view.addSubview(imageView)
+        // WebView - 예고편 띄우기
+        let webView: WKWebView = WKWebView()
+        let urlString: String = "https://www.youtube.com/embed/dlSkyNLOncY?si=_3rNXJGII6TbwHvE"
+        let url: URL = URL(string: urlString)!
+        let urlRequset: URLRequest = URLRequest(url: url)
+        webView.load(urlRequset)
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        webView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        
+        // ScrollView - WebView를 제외한 나머지 컨텐츠 담기
+        let mainScrollView = UIScrollView()
+        mainScrollView.backgroundColor = .systemPink
+        mainScrollView.contentSize = CGSize(width: Int(view.frame.width), height: 800)
+        
+        // MainStackView
+        let mainStackView = UIStackView()
+        mainStackView.axis = .vertical
+        mainStackView.alignment = .fill
+        mainStackView.distribution = .fill
+        mainStackView.spacing = 10
+        mainStackView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(mainStackView)
+        
+        mainStackView.addArrangedSubview(webView)
+        mainStackView.addArrangedSubview(mainScrollView)
+        
+        // Label - 작품 1줄 설명
+        let summaryWorkLabel = UILabel()
+        summaryWorkLabel.text = "마법사의 전설이 시작된다."
+        summaryWorkLabel.font = .systemFont(ofSize: 30, weight: .bold)
+        
+        // Label - 작품 디테일 설명
+        let detailWorkLabel = UILabel()
+        detailWorkLabel.numberOfLines = 0
+        detailWorkLabel.text = "평점: 8.5 / 10\n\n개봉일: 2001. 11.\n\n감독: 크리스 콜럼버스\n\n출연진: 다니엘 레드클리프, 엠마 왓슨, 이선준 외"
+        detailWorkLabel.font = .systemFont(ofSize: 15, weight: .bold)
+        
+        // Label - 작품 시리즈 포스터
+        let posterLabel = UILabel()
+        posterLabel.text = "해리포터의 정주행 순서"
+        posterLabel.font = .systemFont(ofSize: 30, weight: .bold)
+        
+        // ScrollView - Poster 담기 위한 ScrollView
+        let posterScrollView = UIScrollView()
+        posterScrollView.backgroundColor = .gray
+        posterScrollView.contentSize = CGSize(width: 500, height: 50)
+        
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "gamsangpyung")
+        imageView.backgroundColor = .brown
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        // SubStackView
+        let subStackView = UIStackView()
+        subStackView.axis = .vertical
+        subStackView.alignment = .fill
+        subStackView.distribution = .fill
+        subStackView.spacing = 10
+        subStackView.backgroundColor = .blue
+        subStackView.frame = CGRect(x: 0, y: 0, width: Int(view.frame.width), height: 500)
+        
+        subStackView.addArrangedSubview(summaryWorkLabel)
+        subStackView.addArrangedSubview(detailWorkLabel)
+        subStackView.setCustomSpacing(30, after: detailWorkLabel)
+        subStackView.addArrangedSubview(posterLabel)
+        subStackView.addArrangedSubview(posterScrollView)
+        subStackView.setCustomSpacing(30, after: posterScrollView)
+        subStackView.addArrangedSubview(imageView)
         
         NSLayoutConstraint.activate([
-            imageView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
-            imageView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
-            imageView.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: -100.0),
-            imageView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor),
+            mainStackView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            mainStackView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor),
+            mainStackView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            mainStackView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
+            
+            imageView.heightAnchor.constraint(equalToConstant: 100)
         ])
-        
-        let webView: WKWebView = WKWebView(frame: CGRect(x: 0, y: 100, width: Int(view.frame.width), height: 180))
-        
-        view.addSubview(webView)
-        
-        let urlString: String = "https://www.youtube.com/embed/dlSkyNLOncY?si=_3rNXJGII6TbwHvE"
-        
-        let url: URL = URL(string: urlString)!
-        
-        let urlRequset: URLRequest = URLRequest(url: url)
-        
-        webView.load(urlRequset)
-        
-    }
     
-
-
+        mainScrollView.addSubview(subStackView)
+    }
 }
